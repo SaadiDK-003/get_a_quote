3428cbf5fc49af01daae2c88a4a04bbcbb8aec8f
SaadiDK-003 <pcremote059@gmail.com>
2023-06-18 18:55:49 +0500

    config file update

--- config.php
+++ config.php
@@ -1,14 +1,23 @@
 <?php
 // Site URL
-$url = (empty($_SERVER['HTTPS']) ? 'http' : 'https')."://$_SERVER[HTTP_HOST]/get_a_quote/";
+$host = $_SERVER['HTTP_HOST'];
 
-// Database Credentials
+if($host == 'localhost'){
+$url = (empty($_SERVER['HTTPS']) ? 'http' : 'https')."://$_SERVER[HTTP_HOST]/get_a_quote/";
+// Database Credentials LOCAL
 define('HOST','localhost');
 define('USER','root');
 define('PWD','');
-define('DB','get_a_quote');
+define('DB','get_a_quote');   
+} else {
+$url = (empty($_SERVER['HTTPS']) ? 'http' : 'https')."://$_SERVER[HTTP_HOST]/clientarea/";
+// Database Credentials LIVE
+define('HOST','localhost');
+define('USER','octoinsurance_admin');
+define('PWD','O3x6q6;tFPy2');
+define('DB','octoinsurance_quote');
+}
 
 // Global Usage Variables
 define('site_url',$url);
 define('website_title','YOUR TITLE HERE');
-?>
